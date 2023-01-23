280846de80f9b9d52dee36fc3e6f7d9f03e4ce2c
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-01-23 11:09:43 +0300

    complited

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,32 @@
+import React,{ useState,useEffect} from 'react'
+import GifList from './GifList'
+import GifSearch from './GifSearch'
+const GifListContainer = () => {
+
+    const[gif, setGif] = useState([])
+    const [search, setSearch] = useState("dolphins");
+    
+    // const apiKey='1iwYJT1yONNHCIeXGdp7ZHi5KcGBZKYx';
+    const apiKey='U97b8mxCijlD4h3it1Tt3D79mfvvq3nX';
+
+    useEffect(()=>{
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}&rating=g`)
+        .then((res)=>res.json())
+        .then(({data})=>{
+            const gifs = data.slice(0,3).map((gif) => ({ url: gif.images.original.url }));
+            setGif(gifs);
+            
+        })
+    
+    },[search])
+
+
+  return (
+    <div style={{display:'flex',justifyContent:'space-around'}}>
+     <GifList  gifs={gif} />
+      <GifSearch onSubmitQuery={setSearch} />
+    </div>
+  )
+}
+
+export default GifListContainer
